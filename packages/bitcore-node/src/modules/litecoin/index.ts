0520dcbd53ce644d78a3d9bad77c2c4058405b14
Eugene Sinitsyn <sinitsyn.e@n-t.io>
2019-10-11 11:51:39 +0300

    Add LTC Support

---
+++ packages/bitcore-node/src/modules/litecoin/index.ts
@@ -0,0 +1,13 @@
+import { BitcoinP2PWorker } from '../bitcoin/p2p';
+import { BaseModule } from "..";
+import { LTCStateProvider } from "../../providers/chain-state/ltc/ltc";
+
+export default class BitcoinModule extends BaseModule {
+  constructor(services: BaseModule["bitcoreServices"]) {
+    super(services);
+    services.Libs.register('LTC', 'litecore-lib', 'litecore-p2p');
+    services.P2P.register('LTC', BitcoinP2PWorker);
+    services.CSP.registerService('LTC', new LTCStateProvider());
+  }
+}
+
