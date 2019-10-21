cc6f8c4e448b7e1f68853bfd4fe0cb46542e3fd4
Eugene Sinitsyn <sinitsyn.e@n-t.io>
2019-10-21 14:21:34 +0300

    Fix litecoin module name

--- packages/bitcore-node/src/modules/litecoin/index.ts
+++ packages/bitcore-node/src/modules/litecoin/index.ts
@@ -2,7 +2,7 @@ import { BitcoinP2PWorker } from '../bitcoin/p2p';
 import { BaseModule } from "..";
 import { LTCStateProvider } from "../../providers/chain-state/ltc/ltc";
 
-export default class BitcoinModule extends BaseModule {
+export default class LitecoinModule extends BaseModule {
   constructor(services: BaseModule["bitcoreServices"]) {
     super(services);
     services.Libs.register('LTC', 'litecore-lib', 'litecore-p2p');
